9b34cf4798afe4a1c9c6a2e525272ab8f23c1bee
Tony Ojeda <tojeda96@gmail.com>
2020-11-07 06:11:55 -0500

    feature: handler error

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video al productor']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internalr_server_error(error):
+    return render_template('500.html',error=error)
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr

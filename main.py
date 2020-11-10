2a9b60d430d3ec4dc0c870f955d105489647f26f
Tony Ojeda <tojeda96@gmail.com>
2020-11-10 06:24:28 -0500

    feature: notifications with flash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -52,6 +52,8 @@ def hello():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session['username'] = username
+        
+        flash('Nombre de usuario registrado con éxito!')
 
         return redirect(url_for('index'))
 
